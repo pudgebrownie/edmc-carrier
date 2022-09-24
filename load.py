ee3d87a9e83a447869a001dc58c67298d9a0d3a8
jklokocso <jakeklokocso@gmail.com>
2022-09-23 22:22:41 -0700

    Added option for including the targeted planetary body in the destination name

--- load.py
+++ load.py
@@ -9,6 +9,7 @@ from notifications import CarrierNotificationDispatcher
 WEBHOOK_URL_CONFIG_KEY = 'edmc-carrier.webhook_url'
 CARRIER_NAME_CONFIG_KEY = 'edmc-carrier.carrier_name'
 CARRIER_IMAGE_URL_CONFIG_KEY = 'edmc-carrier.carrier_image_url'
+USE_BODY_INFO_CONFIG_KEY = 'edmc-carrier.use_body_info'
 
 this = sys.modules[__name__]
 carrier_notification_dispatcher = CarrierNotificationDispatcher()
@@ -20,10 +21,12 @@ def plugin_start3(plugin_dir):
     webhook_url = __load_config(WEBHOOK_URL_CONFIG_KEY)
     carrier_name = __load_config(CARRIER_NAME_CONFIG_KEY)
     carrier_image_url = __load_config(CARRIER_IMAGE_URL_CONFIG_KEY)
+    this.use_body_info = __load_config(USE_BODY_INFO_CONFIG_KEY)
 
     carrier_notification_dispatcher.__setattr__('webhook_url', webhook_url)
     carrier_notification_dispatcher.__setattr__('carrier_name', carrier_name)
     carrier_notification_dispatcher.__setattr__('carrier_image_url', carrier_image_url)
+    carrier_notification_dispatcher.__setattr__('use_body_info', this.use_body_info)
    
     return "EDMC-Carrier"
 
@@ -53,31 +56,48 @@ def plugin_prefs(parent, cmd, is_beta):
     this.prefs_carrier_image_url.insert(tk.END, carrier_image_url)
     this.prefs_carrier_image_url.grid(row=3, column=1, padx=10, pady=2, sticky=tk.EW)
 
-    return frame
+    if not __load_config(USE_BODY_INFO_CONFIG_KEY):
+        this.use_body_info = tk.IntVar(value = 0)
+    else:
+        this.use_body_info = tk.IntVar(value = 1)
 
+    this.prefs_use_body_info_button = nb.Checkbutton(
+            frame,
+            text = "Include Body Name",
+            variable = this.use_body_info
+        )
+    this.prefs_use_body_info_button.grid(row=4, padx=10, pady=2, sticky=tk.W)
+
+    return frame
 
 def prefs_changed(cmdr, is_beta):
     new_webhook_url = this.prefs_webhook_url.get().strip()
     new_carrier_name = this.prefs_carrier_name.get().strip()
     new_carrier_image_url = this.prefs_carrier_image_url.get().strip()
+    new_use_body_info = this.use_body_info.get()
 
     carrier_notification_dispatcher.__setattr__('webhook_url', new_webhook_url)
     carrier_notification_dispatcher.__setattr__('carrier_name', new_carrier_name)
     carrier_notification_dispatcher.__setattr__('carrier_image_url', new_carrier_image_url)
+    carrier_notification_dispatcher.__setattr__('use_body_info', new_use_body_info)
 
     config.set(WEBHOOK_URL_CONFIG_KEY, new_webhook_url)
     config.set(CARRIER_NAME_CONFIG_KEY, new_carrier_name)
     config.set(CARRIER_IMAGE_URL_CONFIG_KEY, new_carrier_image_url)
+    config.set(USE_BODY_INFO_CONFIG_KEY, new_use_body_info)
 
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
     event_name = entry.get("event")
+
+    if this.use_body_info:
+        destination = entry.get('Body')
+    else:
+        destination = entry.get('SystemName')
     
     if event_name == 'CarrierJumpRequest':
-        curr_system = system
-        dest_system = entry.get('SystemName')
         timestamp = entry.get('timestamp')
-        carrier_notification_dispatcher.send_jump_request_notification(cmdr, curr_system, dest_system, timestamp)
+        carrier_notification_dispatcher.send_jump_request_notification(cmdr, destination, timestamp)
 
 
     if event_name == "CarrierJumpCancelled":
